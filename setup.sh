136afcf302662a62e498893f4296e5e1ba172ace
Odyssee <otremoulis@gmail.com>
2020-09-01 12:16:44 +0200

    Generate requirements from pipreqs

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+apt update
+
+apt install git python3-pip #wget
+
+#wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+#bash Miniconda3-latest-Linux-x86_64.sh
+
+git clone https://github.com/houseofai/image-captioning.git
+cd image-captioning
+
+pip install -r requirements.txt
